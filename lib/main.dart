2636cddbe59ba9977c77b0492d0905134c2bb79e
Karem Mohamed <karemkero21@gmail.com>
2019-12-10 17:47:56 +0200

    Creating App Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }
 
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fashion Shop'),
+        backgroundColor: Colors.blue,
+        actions: <Widget>[
+
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // Search Pressed
+            },
+          ),
+
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // Shopping Cart Pressed
+            },
+          )
+
+        ],
+      ),
+    );
+  }
+}
